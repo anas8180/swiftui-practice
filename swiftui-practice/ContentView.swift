3ec29a3faa68cefee32aadaf84b60e1d1916bb67
Moohamed Anas <anasrm.anas@gmail.com>
2024-01-09 15:16:26 +0400

    Initial Commit

---
+++ swiftui-practice/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  swiftui-practice
+//
+//  Created by Mohamed Anas on 09/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-practice/swiftui_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_practiceApp.swift
+//  swiftui-practice
+//
+//  Created by Mohamed Anas on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
