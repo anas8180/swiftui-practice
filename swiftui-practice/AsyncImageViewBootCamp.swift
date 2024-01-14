645b56c6e56a0462f2e03cc02e578afb5f5ee94b
ahsan1911 <128485329+ahsan1911@users.noreply.github.com>
2024-01-14 15:38:06 +0400

    Addded Ascyn Image

---
+++ swiftui-practice/AsyncImageViewBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncImageViewBootCamp.swift
+//  swiftui-practice
+//
+//  Created by Anas Mohamed on 14/01/2024.
+//
+
+import SwiftUI
+
+struct AsyncImageViewBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncImageViewBootCamp()
+}
---
+++ swiftui-practice/BackgroundBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundView.swift
+//  swiftui-practice
+//
+//  Created by Anas Mohamed on 14/01/2024.
+//
+
+import SwiftUI
+
+struct BackgroundBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundBootCamp()
+}
